15cd36a56cc5264026ee438f2bec845a63cb9192
Live session user <ubuntu@ubuntu.(none)>
2013-02-03 14:02:58 +0000

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObjventDemo::Application.config.secret_token = '18130dae4010bd6db733a0ea4e413c28b7d797cb0c0f1bc3f6d5fc91b77a61c188598eee631e204b3143d27ae8ae2f74281e3afaf4b1d41fd308742b492dccb1'
